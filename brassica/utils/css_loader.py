eeb8c71c88c6222272d52d90321460ea09455f35
Mastahcode19 <118147111+Mastahcode19@users.noreply.github.com>
2025-06-30 13:11:43 +0700

    Update css_loader.py

--- brassica/utils/css_loader.py
+++ brassica/utils/css_loader.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from pathlib import Path
 
-def load_css(file_path): # Ganti parameter dari file_name menjadi file_path karena sekarang akan menerima Path object atau string absolut
+def load_css(file_name):
     """Memuat file CSS eksternal."""
-    with open(file_path, "r") as f: 
+    with open(file_name, "r") as f:
         css = f.read()
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
