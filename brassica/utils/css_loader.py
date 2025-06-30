c36a514face1486c8fab9e66b5b93a29687689db
Mastahcode19 <118147111+Mastahcode19@users.noreply.github.com>
2025-06-30 10:03:00 +0700

    Delete brassica directory

--- brassica/utils/css_loader.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def load_css(file_name):
-    """Memuat file CSS eksternal."""
-    with open(file_name, "r") as f:
-        css = f.read()
-    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
