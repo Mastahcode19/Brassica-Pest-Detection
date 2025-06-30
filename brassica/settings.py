3bd309774d28b2cd7b24b6c483c72dc56f1c9755
Mastahcode19 <118147111+Mastahcode19@users.noreply.github.com>
2025-06-30 17:41:43 +0700

    Update settings.py

--- brassica/settings.py
+++ brassica/settings.py
@@ -1,27 +1,27 @@
 from pathlib import Path
-import sys
+    import sys
 
-# Mendapatkan path absolut dari file saat ini
-FILE = Path(__file__).resolve()
-ROOT = FILE.parent
+    # Mendapatkan path absolut dari file saat ini
+    FILE = Path(__file__).resolve()
+    ROOT = FILE.parent
 
-# Menambahkan path root ke sys.path jika belum ada
-if ROOT not in sys.path:
-    sys.path.append(str(ROOT))
+    # Menambahkan path root ke sys.path jika belum ada
+    if ROOT not in sys.path:
+        sys.path.append(str(ROOT))
 
-# Ubah path menjadi relatif terhadap working directory saat ini
-ROOT = ROOT.relative_to(Path.cwd())
+    # Ubah path menjadi relatif terhadap working directory saat ini
+    ROOT = ROOT.relative_to(Path.cwd())
 
-# Definisi sumber
-IMAGE = 'Image'
-SOURCES_LIST = [IMAGE]
+    # Definisi sumber
+    IMAGE = 'Image'
+    SOURCES_LIST = [IMAGE]
 
-# Konfigurasi gambar
-IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'hamanon_label.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'hama_label1.jpg'
-UPLOADED_IMAGES_DIR = IMAGES_DIR / 'uploaded'
+    # Konfigurasi gambar
+    IMAGES_DIR = ROOT / 'images'
+    DEFAULT_IMAGE = IMAGES_DIR / 'hamanon_label.jpg'
+    DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'hama_label1.jpg'
+    UPLOADED_IMAGES_DIR = IMAGES_DIR / 'uploaded'
 
-# Konfigurasi model ML
-  MODEL_DIR = ROOT / 'weights'
-  DETECTION_MODEL = MODEL_DIR / 'best6.pt'  # Perbarui nama file ke best6.pt
+    # Konfigurasi model ML
+    MODEL_DIR = ROOT / 'weights'
+    DETECTION_MODEL = MODEL_DIR / 'best6.pt'  # Pastikan nama file sesuai
