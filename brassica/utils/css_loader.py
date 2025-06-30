4362d43ed0c277a11bc13e35930cdf5140860e99
Mastahcode19 <118147111+Mastahcode19@users.noreply.github.com>
2025-06-30 08:27:18 +0700

    Add files via upload

---
+++ brassica/utils/css_loader.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def load_css(file_name):
+    """Memuat file CSS eksternal."""
+    with open(file_name, "r") as f:
+        css = f.read()
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
