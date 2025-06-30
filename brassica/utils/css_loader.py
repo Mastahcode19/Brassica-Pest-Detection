e4e5e6c191f122e368581cfe72a9df2522a98a29
Mastahcode19 <118147111+Mastahcode19@users.noreply.github.com>
2025-06-30 14:10:53 +0700

    Update css_loader.py

--- brassica/utils/css_loader.py
+++ brassica/utils/css_loader.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from pathlib import Path
 
 def load_css(file_name):
     """Memuat file CSS eksternal."""
-    with open(file_name, "r") as f:
+    file_path = Path(__file__).parent.parent / file_name
+    if not file_path.exists():
+        st.error(f"File CSS tidak ditemukan di: {file_path}")
+        return
+    with open(file_path, "r") as f:
         css = f.read()
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
